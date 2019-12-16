2029edc34bbc85eb458dacf4b594c09bffcff893
dmolony3 <dmolony@emory.edu>
2019-12-16 10:52:35 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# About
+This repository can be used to generate images for use with [tensorflow projector](http://projector.tensorflow.org/). Note this repository just compresses the raw image to the desired vector size and does not actually generate a feature space embedding. 
+
+# Use
+In create_embedding.py specify the following:
+**directory**: Directory where images are stored
+** data_file**:Path to a file which contains each image filename (no need to include the absolute file path)
+**num_image_rows**: This number sets the number of images in each row in the sprite image (see example sprite image below). This is the square root of the total number of images
+**image_dim**: The dimension of the images contained within the sprite image. Recommended to not exceed 64x64.
+
+Edit *config.json* to set the correct value for the following
+**tensorShape**: Should be [num_images,  image_dim<sup>2</sup>]. 
+**singleImageDim**: The size of single image in the sprite image
+Paths to me the *vecs.tsv*, *metadata.tsv* and *sprite_image.jpg*
+
+Finally, time to let the projector [run](http://projector.tensorflow.org/?config=https://raw.githubusercontent.com/dmolony3/projector_embedding/master/config.json). 
+
+# Acknowledgements
+Inspired by the the following repository
+[https://github.com/anthonySegura/facenet_projections](https://github.com/anthonySegura/facenet_projections)
---
+++ create_embedding.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+import os
+import numpy as np
+from PIL import Image
+
+class DataReader():
+    """Reads images from text file"""
+    def __init__(self, directory, batch_size):
+        self.directory = directory
+        self.batch_size = batch_size
+
+    def read_files(self, file_path):
+        file_list = []
+        f = open(file_path, 'r')
+        for line in f:
+            if line:
+                file_list.append(os.path.join(self.directory, line.strip()))
+        f.close()
+        return file_list
+
+    def decode_image(self, image):
+        image = tf.io.read_file(image)
+        image = tf.io.decode_png(image, channels=1)
+        return image
+
+    def read_batch(self, file_path):
+        file_list = self.read_files(file_path)
+        data = tf.data.Dataset.from_tensor_slices((file_list))
+        #file_list = tf.data.Dataset.list_files(str(file_path/'*/*'))
+        data = data.map(self.decode_image)
+        data = data.batch(batch_size=self.batch_size, drop_remainder=False)
+        return data
+
+
+directory = '/home/microway/Documents/SPADE/results/IVUS_45MHz/inference_latest/images/synthesized_image'
+data_file = '/home/microway/Documents/SPADE/results/IVUS_45MHz/inference_latest/images/synthesized_image/generated.txt'
+num_image_rows = 64 # must be divisble by batch_size
+image_dim = 64
+
+sprite_file = 'sprite_image' + '_' + str(image_dim) + 'x' + str(image_dim) +'.jpg'
+vecs_file = 'vecs' + '_' + str(image_dim) + 'x' + str(image_dim) +'.tsv'
+meta_file = 'metadata' + '_' + str(image_dim) + 'x' + str(image_dim) +'.tsv'
+
+batch_size = 1
+num_images = num_image_rows**2
+
+data = DataReader(directory, batch_size)
+data_iterator = iter(data.read_batch(data_file))
+
+image_list = []
+f1 = open(vecs_file, 'a', encoding='utf-8')
+f2 = open(meta_file, 'a', encoding='utf-8')
+for i in range(num_images):
+    batch = next(data_iterator)
+    batch_downsampled = tf.image.resize(batch, [image_dim, image_dim])
+    batch_vectorized = tf.reshape(batch_downsampled, shape=[batch_size, image_dim*image_dim])
+    batch_vectorized = tf.squeeze(batch_vectorized, 0)
+    dim = batch_vectorized.shape
+    f1.write('\t'.join([str(batch_vectorized.numpy()[j]) for j in range(dim[0])]) + "\n")
+    f2.write('{}\n'.format(1)) # set label to 1 for all images
+    image_list.append(tf.squeeze(tf.squeeze(batch_downsampled, 0), -1))
+f1.close()
+f2.close()
+
+# generate the sprite image
+sprite_image = np.zeros((image_dim*num_image_rows, image_dim*num_image_rows))
+idx = 0
+for i in range(num_image_rows):
+    for j in range(num_image_rows):
+        sprite_image[i*image_dim:(i+1)*image_dim, j*image_dim:(j+1)*image_dim] = image_list[idx]
+        idx += 1
+im = Image.fromarray(sprite_image.astype(np.uint8))
+im.save(sprite_file)
+
